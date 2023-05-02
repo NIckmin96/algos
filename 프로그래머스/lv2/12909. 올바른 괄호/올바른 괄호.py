76dc66968035bb8ac81ad31ef10637b1b398259d
Min Bokki(BK) <81205952+NIckmin96@users.noreply.github.com>
2023-05-02 15:03:20 +0900

    [level 2] Title: 올바른 괄호, Time: 6.71 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,20 @@
+def solution(s):
+    if s[0]==')':
+        return False
+    
+    cnt = 0
+    for i in s:
+        if i=='(':
+            cnt+=1
+        else:
+            cnt-=1
+        if cnt < 0:
+            return False
+            
+    if cnt == 0:
+        return True
+    else:
+        return False
+    
+
+    
