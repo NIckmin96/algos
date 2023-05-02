687143f29031ecc467335ec817d55bb03a5ddeb3
Min Bokki(BK) <81205952+NIckmin96@users.noreply.github.com>
2023-05-02 12:15:28 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT animal_id, name, IF(sex_upon_intake like "Neutered%" or sex_upon_intake like "Spayed%","O","X") as "중성화" from animal_ins
+    order by animal_id
+    
+# select animal_id, name, sex_upon_intake from animal_ins
+#     order by animal_id
