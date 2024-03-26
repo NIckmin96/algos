2748e4da6fd07c5b98bbe2cf46996902dc24f8be
Min Bokki(BK) <81205952+NIckmin96@users.noreply.github.com>
2024-03-26 19:40:03 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,11 @@
+def solution(n,a,b):
+    answer = 1
+    import math
+    while True:
+        if math.ceil(a/2)==math.ceil(b/2):
+            break
+        a = math.ceil(a/2)
+        b = math.ceil(b/2)
+        answer+=1
+
+    return answer
