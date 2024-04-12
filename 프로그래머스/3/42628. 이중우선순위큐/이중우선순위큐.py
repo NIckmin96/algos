92f8d3e6ea8c9aa6ff7659a589c3bd46a67c1f65
Min Bokki(BK) <81205952+NIckmin96@users.noreply.github.com>
2024-04-12 22:44:00 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,28 +1,38 @@
 def solution(operations):
     answer = []
     import heapq
-    # min_heap = []
-    # max_heap = []
-    lst = []
+    min_heap = []
+    max_heap = []
+    # lst = []
     for op in operations:
         a,b = op.split()
         if a=='I':
-            lst.append(int(b))
-        #     heapq.heappush((-int(b),int(b)), max_heap)
-        #     heapq.heappush((int(b),int(b)), min_heap)
+            # lst.append(int(b))
+            heapq.heappush(max_heap, -int(b))
+            heapq.heappush(min_heap, int(b))
         else:
-            if lst:
+            if (max_heap!=[])&(min_heap!=[]):
                 if b=='1':
-                    lst = sorted(lst, key=lambda x:-x)[1:]
-                    # heapq.heappop(max_heap)
+                    # lst = sorted(lst, key=lambda x:-x)[1:]
+                    heapq.heappop(max_heap)
+                    min_heap = list(map(lambda x:-x, max_heap))
+                    heapq.heapify(min_heap)
                 else:
-                    lst = sorted(lst)[1:]
-        # print(lst)
-                    
-    if lst:
-        answer.append(max(lst))
-        answer.append(min(lst))
+                    # lst = sorted(lst)[1:]
+                    heapq.heappop(min_heap)
+                    max_heap = list(map(lambda x:-x, min_heap))
+                    heapq.heapify(max_heap)
+        # print(max_heap, min_heap)
+    
+    if (min_heap!=[])&(max_heap!=[]):
+        answer = [-heapq.heappop(max_heap), heapq.heappop(min_heap)]
     else:
         answer = [0,0]
+                    
+    # if lst:
+        # answer.append(max(lst))
+        # answer.append(min(lst))
+    # else:
+        # answer = [0,0]
         
     return answer
