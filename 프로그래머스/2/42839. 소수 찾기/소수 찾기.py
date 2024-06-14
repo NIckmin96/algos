4faa1f1a00a51e59b5612098e2eff8364a0502ca
Min Bokki(BK) <81205952+NIckmin96@users.noreply.github.com>
2024-06-14 16:24:12 +0900

    [level 2] Title: 소수 찾기, Time: 0.98 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -9,14 +9,27 @@ def solution(numbers):
                 return False
         return True
     
-    # 1. permutations
-    from itertools import permutations
-    for i in range(1,len(numbers)+1):
-        s = set(list(map(lambda x:int(''.join(x)), permutations(numbers,i))))
-        for num in s:
-            if is_prime(num):
-                answer.add(num)
+    # # 1. permutations
+    # from itertools import permutations
+    # for i in range(1,len(numbers)+1):
+    #     s = set(list(map(lambda x:int(''.join(x)), permutations(numbers,i))))
+    #     for num in s:
+    #         if is_prime(num):
+    #             answer.add(num)
+                
+    # 2. 재귀
+    def permutation(numbers,k,res=''):
+        if len(res)==k:
+            if is_prime(int(res)):
+                answer.add(int(res))
+                return
+        for _ in range(len(numbers)):
+            num = numbers.pop(0)
+            permutation(numbers,k,res+num)
+            numbers.append(num)
     
+    for i in range(1,len(numbers)+1):
+        permutation(numbers,i)
         
                             
     return len(answer)
