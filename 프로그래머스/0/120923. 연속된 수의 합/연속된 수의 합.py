52d4a75b7755a02e37dc71dafd24e7042d16d2c2
Min Bokki(BK) <81205952+NIckmin96@users.noreply.github.com>
2024-10-16 00:19:17 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,13 +1,18 @@
 def solution(num, total):
-    answer = []
-    if total%num==0:
-        middle = total//num
-        for i in range(middle-num//2, middle+num//2+1):
-            answer.append(i)
-    else:
-        middle = total/num
-        left = int(middle-0.5)
-        right = int(middle+0.5)
-        for i in range(left-num//2+1, right+num//2):
-            answer.append(i)
+    # 수식적 풀이
+    start = total/num-(num-1)/2
+    answer = [start+i for i in range(num)]
+        
+    
+    # answer = []
+    # if total%num==0:
+    #     middle = total//num
+    #     for i in range(middle-num//2, middle+num//2+1):
+    #         answer.append(i)
+    # else:
+    #     middle = total/num
+    #     left = int(middle-0.5)
+    #     right = int(middle+0.5)
+    #     for i in range(left-num//2+1, right+num//2):
+    #         answer.append(i)
     return answer
