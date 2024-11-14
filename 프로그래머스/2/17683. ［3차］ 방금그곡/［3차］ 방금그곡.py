bde67955071897b9d0bf961567736454069939f6
Min Bokki(BK) <81205952+NIckmin96@users.noreply.github.com>
2024-11-14 22:59:38 +0900

    [level 2] Title: [3차] 방금그곡, Time: 19.35 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -0,0 +1,61 @@
+def solution(m, musicinfos):
+    from collections import deque
+    answer = '(None)'
+    max_len = 0
+    
+    # m정확히 구하기(# 구분)
+    m_cp = deque(list(m).copy())
+    m = []
+    while m_cp:
+        tmp = m_cp.popleft()
+        if tmp=='#':
+            m[-1] = ''.join([m[-1],tmp])
+        else:
+            m.append(tmp)
+    len_m = len(m)    
+    str_m = ''.join(m)
+    
+    # main
+    for music in musicinfos:
+        start, end, name, melody = music.split(',')
+        _h,_m = start.split(':')
+        start = int(_h)*60+int(_m)
+        _h,_m = end.split(':')
+        end = int(_h)*60+int(_m)
+        t_diff = end-start
+        
+        # melody길이 정확히 구하기(# 붙은 경우 구분)
+        melody_cp = deque(list(melody).copy())
+        melody = []
+        while melody_cp:
+            tmp = melody_cp.popleft()
+            if tmp=='#':
+                melody[-1] = ''.join([melody[-1],tmp])
+            else:
+                melody.append(tmp)
+        len_melody=len(melody)
+        
+        # radio에서 들은 멜로디 복원
+        if t_diff<len_melody:
+            radio=melody[:t_diff]
+        else:
+            div,mod = divmod(t_diff,len_melody)
+            radio=melody*div+melody[:mod]
+        
+        # radio에서 들은 멜로디와 기억하는 멜로디가 일치하는지 확인
+        for i in range(0,len(radio)-len(m)+1):
+            str_radio = ''.join(radio[i:i+len(m)])
+            if str_m==str_radio:
+                if answer!='(None)':
+                    if max_len<t_diff: # 재생 시간이 긴 경우, update
+                        max_len = t_diff
+                        answer = name
+                    else: # 재생 시간 같거나 짧은 경우 현재 answer 유지
+                        pass
+                        
+                else: # 처음인 경우
+                    max_len = t_diff
+                    answer = name
+                break
+        
+    return answer
