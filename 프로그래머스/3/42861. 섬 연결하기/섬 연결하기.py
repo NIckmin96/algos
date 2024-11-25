edf179dbf64c1f1e2d6fa8bedd3d0765b96db6ee
Min Bokki(BK) <81205952+NIckmin96@users.noreply.github.com>
2024-11-25 22:05:51 +0900

    [level 3] Title: 섬 연결하기, Time: 0.09 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,31 +1,27 @@
-# x의 부모 노드가, x일때 까지 재귀 호출 -> 처음 사용된 x의 부모노드 return됨
+# kruskal
 def find_parent(parent, x):
-    if parent[x]!=x: 
-        parent[x] = find_parent(parent, parent[x])
+    if parent[x]!=x:
+        parent[x] = find_parent(parent,parent[x])
     return parent[x]
 
-# 두 노드가 속한 집합을 합치기
-def union_parent(parent, a, b):
-    a = find_parent(parent, a) # a의 root node
-    b = find_parent(parent, b) # b의 root node
-    if a<b:
+def union(parent,a,b):
+    if parent[a]<=parent[b]:
         parent[b] = a
     else:
         parent[a] = b
-    
+
 def solution(n, costs):
     answer = 0
-    # 크루스칼 알고리즘
-    parent = [0]*n
-    # parent 초기화
-    for i in range(n):
-        parent[i]=i
-    # cost작은 순서대로 오름차순 정렬
+    # 0. parent(root 노드) 초기화
+    parent = {i:i for i in range(n)}
+    # 1. cost별로 정렬
     costs = sorted(costs, key=lambda x:x[-1])
-    # root노드 비교
+    # 2. 순차적으로 탐색하면서, cycle이 생성 안되는 경우에만 추가
     for a,b,c in costs:
-        if find_parent(parent,a)!=find_parent(parent,b):
-            union_parent(parent,a,b)
+        a = find_parent(parent,a)
+        b = find_parent(parent,b)
+        if a!=b:
+            union(parent, a, b)
             answer+=c
     
     return answer
