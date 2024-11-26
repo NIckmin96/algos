e8f2cf6d98051224c162564725fbdf890de21709
Min Bokki(BK) <81205952+NIckmin96@users.noreply.github.com>
2024-11-26 21:54:34 +0900

    [level 3] Title: 불량 사용자, Time: 98.56 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,23 +1,37 @@
-def solution(user_id, banned_id):
-    # 똑같은 ban id가 여러개 있는 경우
-    # ban id는 다른데 똑같은 id가 걸리는 경우
-        # [**o*o, ***do] -> [[frodo, crodo],[frodo,crodo]]
-    answer=0
-    import re
-    from itertools import permutations
-    # 모든 조합 구하기
-    answers = []
-    for p in permutations(user_id, len(banned_id)):
-        cnt = 0
-        for i in range(len(p)):
-            ban = banned_id[i].replace('*','.')
-            if re.match(ban,p[i]) and len(ban)==len(p[i]):
-                cnt+=1
-        if cnt==len(banned_id):
-            p = sorted(p)
-            if p not in answers:
-                answers.append(p)
-                
-    answer = len(answers)
+def check(uid, bid):
+    if len(uid)!=len(bid):
+        return False
     
+    for i in range(len(bid)):
+        if uid[i]!=bid[i] and bid[i]!='*':
+            return False
+        
+    return True
+
+def dfs(idx=0,lst=[]):
+    if len(lst)==len(bannable):
+        res.add(tuple(sorted(lst)))
+        return 
+    elif idx>=len(bannable):
+        return
+    
+    for _id in bannable[idx]:
+        if _id not in lst:
+            dfs(idx+1,lst+[_id])
+
+def solution(user_id, banned_id):
+    global res, bannable
+    answer = 0
+    bannable = []
+    for bid in banned_id:
+        lst = []
+        for uid in user_id:
+            if check(uid, bid):
+                lst.append(uid)
+        bannable.append(lst)
+    # print(bannable)
+    res = set()
+    dfs()
+    answer = len(res)
+                
     return answer
